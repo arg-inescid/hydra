6957df3e59ea41215f2a2199f79334b503182926
jovanstevanovic <jovan.stevanovic1@outlook.com>
2021-03-20 19:20:25 +0100

    Tools initial commit

---
+++ tools/run-plot.py
@@ -0,0 +1,186 @@
+#!/usr/bin/python
+import enum
+import json
+import os
+import random
+import re
+import string
+import subprocess
+import sys
+from json import JSONDecodeError
+from time import sleep
+
+from PIL import Image
+from pygnuplot import gnuplot
+
+
+# Message type.
+class MessageType(enum.Enum):
+    INFO = "[INFO]"
+    ERROR = "[ERR0]"
+    WARN = "[WARN]"
+    SPEC = "[SPEC]"
+
+
+# Global variable
+filter_mapping = {
+    "latency": "Time taken for tests",
+    "transfer-rate": "Requests per second",
+    "memory": "Nothing for now."
+}
+label_mapping = {
+    "latency": "Time consumption (ms)",
+    "transfer-rate": "Requests per second (#/sec)",
+    "memory": "Nothing for now."
+}
+title_mapping = {
+    "latency": "Time consumption",
+    "transfer-rate": "Requests per second",
+    "memory": "Nothing for now."
+}
+
+
+# Util methods.
+def print_message(message, t):
+    if t == MessageType.INFO:
+        print('\033[1;39m' + t.value + " " + message + '\033[0m')  # default
+        return
+    if t == MessageType.ERROR:
+        print('\033[1;31m' + t.value + " " + message + '\033[0m')  # red
+        return
+    if t == MessageType.WARN:
+        print('\033[1;33m' + t.value + " " + message + '\033[0m')  # yellow
+        return
+    if t == MessageType.SPEC:
+        print('\033[1;32m' + t.value + " " + message + '\033[0m')  # green
+        return
+    print(message)
+
+
+def install_required_packages():
+    required_packages = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
+    installed_packages = [r.decode().split('==')[0] for r in required_packages.split()]
+    print_message("Installed packages: [ " + " ".join(installed_packages) + " ]", MessageType.WARN)
+
+
+# File util methods.
+def load_data(filename):
+    try:
+        with open(filename) as input_file:
+            return json.load(input_file)
+    except IOError:
+        print_message("Input file {} is missing or deleted!".format(filename), MessageType.ERROR)
+        exit(1)
+    except JSONDecodeError as err:
+        print_message("Bad JSON syntax: {}".format(err), MessageType.ERROR)
+        exit(1)
+
+
+def read_file(filename):
+    try:
+        with open(filename) as input_file:
+            return input_file.read()
+    except IOError:
+        print_message("Input file {} is missing or deleted!".format(filename), MessageType.WARN)
+    return ""
+
+
+def write_file(filename, output):
+    try:
+        with open(filename, 'w') as output_file:
+            output_file.write(output)
+        return True
+    except IOError:
+        print_message("Error during writing in output file {}!".format(filename), MessageType.WARN)
+        return False
+
+
+def filter_file(filter_type, input_files):
+    if len(input_files) == 0:
+        print_message("No data to be plotted for {}!".format(filter_type), MessageType.WARN)
+        return None
+    outputs = []
+    titles = []
+    regex = re.compile("{}: *([0-9.]*)".format(filter_mapping[filter_type]))
+    for input_file in input_files:  # 0: plot title, 1: plot data
+        if len(input_file) < 2:
+            print_message("Insufficient number of arguments for plot command ({})!".format(len(input_file)),
+                          MessageType.WARN)
+            continue
+        file = read_file(input_file[1])
+        if len(file) == 0:
+            continue
+        outputs.append(regex.findall(file))
+        titles.append(input_file[0])
+    return outputs, titles
+
+
+def generate_tmp_files(results):
+    letters = string.ascii_lowercase
+    outputs = []
+    for result in results:
+        filename = ''.join(random.choice(letters) for _ in range(10)) + ".tmp"
+        success = write_file(filename, '\n'.join(result))
+        if success:
+            outputs.append(filename)
+    return outputs
+
+
+def remove_tmp_files(files):
+    for file in files:
+        os.remove(file)
+
+
+# Core methods.
+def generate_command(data_files, titles):
+    command = ""
+    for index, data_file in enumerate(data_files):
+        command += '"{data_file}" title "{title}" with lines'.format(data_file=data_file, title=titles[index])
+        if index + 1 != len(data_files):
+            command += ", \\ " + '\n'
+    return command
+
+
+def generate_output(result, filter_type, output, titles):
+    g = gnuplot.Gnuplot()
+    g.cmd('''
+            reset
+            set terminal pngcairo enhanced font "Verdana,64" size 4500,3000 linewidth 20
+            set output "{output}"
+            set grid
+            set key out top horizontal
+            set xlabel "Iteration"
+            set ylabel "{ylabel}"
+            set decimalsign locale
+            set yrange [0:]
+          '''.format(output=output, ylabel=label_mapping[filter_type]))
+    tmp_files = generate_tmp_files(result)
+    g.plot(generate_command(tmp_files, titles))
+    sleep(10)   # Wait until plot is finished.
+    remove_tmp_files(tmp_files)
+
+
+def display_image(filename):
+    Image.open(filename).show()
+
+
+def plot(data):
+    print_message("{} is running...".format(data['plot']), MessageType.INFO)
+
+    for p in data['plots']:
+        result, titles = filter_file(p['type'], p['data'])
+        if result:
+            generate_output(result, p['type'], p['output'], titles)
+            if p['show_image']:
+                display_image(p['output'])
+
+    print_message("{} is running...done".format(data['plot']), MessageType.INFO)
+
+
+# Main function.
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print_message("Insufficient number of arguments ({})!".format(len(sys.argv)), MessageType.ERROR)
+        exit(1)
+    install_required_packages()
+    plot(load_data(sys.argv[1]))
---
+++ tools/run-test.py
@@ -0,0 +1,231 @@
+#!/usr/bin/python
+import enum
+import json
+import os
+import random
+import shlex
+import subprocess
+import sys
+import threading
+import time
+from json import JSONDecodeError
+
+import requests
+
+
+# Message type.
+class MessageType(enum.Enum):
+    INFO = "[INFO-{}]"
+    ERROR = "[ERR0-{}]"
+    WARN = "[WARN-{}]"
+    SPEC = "[SPEC-{}]"
+
+
+# Util methods.
+def print_message(username, message, t):
+    if t == MessageType.INFO:
+        print('\033[1;39m' + t.value.format(username) + " " + message + '\033[0m')  # default
+        return
+    if t == MessageType.ERROR:
+        print('\033[1;31m' + t.value.format(username) + " " + message + '\033[0m')  # red
+        return
+    if t == MessageType.WARN:
+        print('\033[1;33m' + t.value.format(username) + " " + message + '\033[0m')  # yellow
+        return
+    if t == MessageType.SPEC:
+        print('\033[1;32m' + t.value.format(username) + " " + message + '\033[0m')  # green
+        return
+    print(message)
+
+
+def install_required_packages():
+    required_packages = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
+    installed_packages = [r.decode().split('==')[0] for r in required_packages.split()]
+    print_message("general", "Installed packages: [ " + " ".join(installed_packages) + " ]", MessageType.WARN)
+
+
+# File util methods.
+def load_data(filename):
+    try:
+        with open(filename) as input_file:
+            return json.load(input_file)
+    except IOError:
+        print_message("general", "Input file {} is missing or deleted!".format(filename), MessageType.ERROR)
+        exit(1)
+    except JSONDecodeError as err:
+        print_message("general", "Bad JSON syntax: {}".format(err), MessageType.ERROR)
+        exit(1)
+
+
+def read_file(username, filename):
+    try:
+        with open(filename, 'rb') as input_file:
+            return input_file.read()
+    except IOError:
+        print_message(username, "Input file {} is missing or deleted!".format(filename), MessageType.WARN)
+
+
+def write_file(username, filename, output):
+    try:
+        with open(filename, 'a') as output_file:
+            output_file.write(output)
+    except IOError:
+        print_message(username, "Error during writing in output file {}!".format(filename), MessageType.WARN)
+
+
+# Core methods.
+def run(username, command):
+    outs, errs = subprocess.Popen(shlex.split(command),
+                                  stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+    outs, errs = outs.decode(sys.stdout.encoding), errs.decode(sys.stdout.encoding)
+    if len(errs) > 0:
+        print_message(username, "Command ({}) error log:\n{}".format(command, errs), MessageType.WARN)
+    return outs
+
+
+def upload_lambdas(username, manager, command):
+    for _ in range(command['num_lambdas']):
+        print_message(username, "Response: " +
+                      requests.post("{manager}/upload_lambda?user={username}&name={lambda_name}"
+                                    .format(manager=manager, username=username, lambda_name=command['lambda_name']),
+                                    headers={'Content-type': 'application/octet-stream'},
+                                    data=read_file(username, command['res_path'])).text, MessageType.INFO)
+
+
+def remove_lambdas(username, manager, lambda_name):
+    print_message(username, "Response: " + requests.post("{manager}/remove_lambda?user={username}&name={lambda_name}"
+                                                         .format(manager=manager, username=username,
+                                                                 lambda_name=lambda_name)).text,
+                  MessageType.INFO)
+
+
+def send(username, manager, command):
+    path = os.path.dirname(command['output_file'])
+    if len(path) > 0:
+        os.makedirs(path, exist_ok=True)
+    if os.path.exists(command['output_file']):
+        os.remove(command['output_file'])
+
+    args_len = len(command['lambda_args'])
+    print_message(username, "Iterating from 1 to {}...".format(command['iterations']), MessageType.INFO)
+    for i in range(command['iterations']):
+        args = "?args=" + command['lambda_args'][random.randint(0, args_len - 1)] if args_len > 0 else ""
+        output = "ITERATION({})...\n".format(i)
+        output += run(username, "ab -n {num_requests} -c {num_clients} {manager}/{username}/{lambda_name}{args}"
+                      .format(num_requests=command['num_requests'], num_clients=command['num_clients'],
+                              manager=manager, username=username, lambda_name=command['lambda_name'],
+                              args=args))
+        output += "ITERATION({})...done\n\n".format(i)
+        write_file(username, command['output_file'], output)
+    print_message(username, "Iterating from 1 to {}...done".format(command['iterations']), MessageType.INFO)
+
+
+def star_sending(username, manager, sending_info):
+    send_threads = []
+    for one_send_info in sending_info:
+        if len(one_send_info) < 6:
+            print_message(username, "Insufficient number of arguments for request command ({})!"
+                          .format(len(one_send_info)), MessageType.WARN)
+            continue
+        send_thread = threading.Thread(target=send, args=(username, manager, {
+            "iterations": one_send_info[0],
+            "num_requests": one_send_info[1],
+            "num_clients": one_send_info[2],
+            "lambda_name": one_send_info[3],
+            "lambda_args": one_send_info[4],
+            "output_file": one_send_info[5]
+        }))
+        send_thread.start()
+        send_threads.append(send_thread)
+
+    for send_thread in send_threads:
+        send_thread.join()
+
+
+def start_user(user, manager):
+    print_message(user['username'], "{} is running...".format(user['username']), MessageType.INFO)
+
+    kindness_counter = 0
+    for command in user['commands']:
+        comm_len = len(command)
+        if comm_len < 1:
+            print_message(user['username'], "Insufficient number of arguments for command ({})!".format(comm_len),
+                          MessageType.WARN)
+            continue
+        command[0].lower()
+        if command[0] == "u" or command[0] == "upload":
+            if comm_len < 4:
+                print_message(user['username'], "Insufficient number of arguments for upload command ({})!"
+                              .format(comm_len), MessageType.WARN)
+                continue
+            upload_lambdas(user['username'], manager, {
+                "num_lambdas": command[1],
+                "lambda_name": command[2],
+                "res_path": command[3],
+            })
+            continue
+        if command[0] == "s" or command[0] == "send":
+            if comm_len < 2:
+                print_message(user['username'], "Insufficient number of arguments for send command ({})!"
+                              .format(comm_len), MessageType.WARN)
+                continue
+            star_sending(user['username'], manager, command[1:])
+            continue
+        if command[0] == "p" or command[0] == "pause":
+            if comm_len < 2:
+                print_message(user['username'], "Insufficient number of arguments for pause command ({})!"
+                              .format(comm_len), MessageType.WARN)
+                continue
+            print_message(user['username'], "Pausing...", MessageType.INFO)
+            time.sleep(command[1])
+            print_message(user['username'], "Pausing...done", MessageType.INFO)
+            continue
+        if command[0] == "r" or command[0] == "remove":
+            if comm_len < 2:
+                print_message(user['username'], "Insufficient number of arguments for remove lambda command ({})!"
+                              .format(comm_len), MessageType.WARN)
+                continue
+            remove_lambdas(user['username'], manager, command[1])
+            continue
+        if command[0] == "k" or command[0] == "kindness":
+            if comm_len < 2:
+                print_message(user['username'], "Insufficient number of arguments for kindness command ({})!"
+                              .format(comm_len), MessageType.WARN)
+                continue
+            kindness_counter += 1
+    if kindness_counter > 0:
+        print_message(user['username'], "Thank you, {}, for your kindness!".format(user['username']), MessageType.SPEC)
+
+    print_message(user['username'], "{} is running...done".format(user['username']), MessageType.INFO)
+
+
+def configure_manager(manager, config_path):
+    print_message("general", "Response: " +
+                  requests.post("{manager}/configure_manager".format(manager=manager),
+                                headers={'Content-type': 'application/json'},
+                                data=read_file("general", config_path)).text, MessageType.INFO)
+
+
+def test(data):
+    print_message("general", "{} is running...".format(data['test']), MessageType.INFO)
+    configure_manager(data['manager'], data['config_path'])
+
+    users = []
+    for user in data['users']:
+        user = threading.Thread(target=start_user, args=(user, data['manager']))
+        user.start()
+        users.append(user)
+
+    for user in users:
+        user.join()
+
+    print_message("general", "{} is running...done".format(data['test']), MessageType.INFO)
+
+
+# Main function.
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print_message("general", "Insufficient number of arguments ({})!".format(len(sys.argv)), MessageType.ERROR)
+        exit(1)
+    install_required_packages()
+    test(load_data(sys.argv[1]))
