aa05e454613437155b3d91fdb3c75ab5724cd441
developer <developer@graalvm-argo>
2021-12-03 16:15:16 +0000

    configs and build script for building truffle NIUK

--- lambda-proxy/src/scripts/build_polyglot_proxy.sh
+++ lambda-proxy/src/scripts/build_polyglot_proxy.sh
@@ -1,16 +1,23 @@
+JAVA_HOME=/home/ubuntu/git/graalvm-argo/argo/resources/graalvm-b655ac5f30-java11-21.3.0-dev
+RES_HOME="/home/ubuntu/git/graalvm-argo/argo/resources"
+TRUFFLE_HOME=$RES_HOME/truffle-build
+PROXY_HOME=$RES_HOME/../lambda-proxy
+
+cd $PROXY_HOME
 ./gradlew clean shadowJar
+cd -
 
-PATH_TO_RESOURCES=""
-VIRTUALIZE_PATH=$PATH_TO_RESOURCES/resources/virtualize.json
+cd $TRUFFLE_HOME
 
-sudo env "PATH=$PATH" native-image \
+sudo env "PATH=$PATH" $JAVA_HOME/bin/native-image \
   --no-fallback \
   --language:js \
   --language:python \
   --features=org.graalvm.argo.lambda_proxy.engine.PolyglotEngineSingletonFeature \
-  -cp build/libs/lambda-proxy-1.0-all.jar \
+  -cp $PROXY_HOME/build/libs/lambda-proxy-1.0-all.jar \
   org.graalvm.argo.lambda_proxy.PolyglotProxy \
   polyglot-proxy \
-  -H:Virtualize="$VIRTUALIZE_PATH" \
+  -H:Virtualize=$RES_HOME/virtualize-polyglot.json \
   -H:+ReportExceptionStackTraces \
-  -H:ConfigurationFileDirectories=src/main/resources/META-INF/native-image/
+  -H:ConfigurationFileDirectories=$TRUFFLE_HOME/META-INF/native-image/
+cd -
