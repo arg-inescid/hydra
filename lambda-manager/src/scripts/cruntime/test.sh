8956aca1adcbb9cf5a243fe0597502a740921b66
Rodrigo Bruno <rbruno@gsd.inesc-id.pt>
2022-06-30 09:00:44 +0100

    Improving cruntime test script.

--- lambda-manager/src/scripts/cruntime/test.sh
+++ lambda-manager/src/scripts/cruntime/test.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
+function DIR {
+	echo "$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+}
+
+ARGO_HOME=$(DIR)/../../../../
+TAP=tap001
+IP=10.0.0.200
+GW=10.0.0.201
+SMASK=16
+
+# Build code to manage vms
 go build start-vm.go
 go build stop-vm.go
 
-sudo ./start-vm -ip 192.168.1.99/24 -gw 192.168.1.83 -tap tap001 -id fc-example2 -img docker.io/library/nginx:1.17-alpine
+# Create tap
+sudo bash $ARGO_HOME/lambda-manager/src/scripts/create_taps.sh $TAP $IP
 
+# Create vm
+sudo ./start-vm -ip $IP/16 -gw $GW -tap $TAP -id fc-example2 -img docker.io/library/nginx:1.17-alpine
+
+# Wait for vm to boot
 sleep 1
 
-curl 192.168.1.99
+# Curl nginx
+curl $IP
 
+# Destroy vm
 sudo ./stop-vm -id fc-example2
+
+# Destroy tap
+sudo bash $ARGO_HOME/lambda-manager/src/scripts/remove_taps.sh $TAP
