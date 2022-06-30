bd4403bafbd5481e4309e395a4eb67099eface54
Rodrigo Bruno <rbruno@gsd.inesc-id.pt>
2022-06-30 08:43:41 +0100

    Creating and deleting taps automatically.

--- lambda-proxy/src/scripts/test-cruntime-nodejs.sh
+++ lambda-proxy/src/scripts/test-cruntime-nodejs.sh
@@ -5,42 +5,36 @@ function DIR {
 }
 
 source $(DIR)/test-shared.sh
+source $(DIR)/test-shared.local
 
 CRUNTIME_HOME=$(DIR)/../../../lambda-manager/src/scripts/cruntime/
 
-IP=10.0.0.165
-MASK=16
-GW=10.0.0.201
-TAP=tap001
-VMID=nodevm5
+TAP=nodejstap
+VMID=nodejsvm
 
 function start_node {
-	sudo $CRUNTIME_HOME/start-vm -ip $IP/$MASK -gw $GW -tap $TAP -id $VMID -img docker.io/openwhisk/action-nodejs-v12:latest
+	sudo bash $ARGO_HOME/lambda-manager/src/scripts/create_taps.sh $TAP $ip
+	sudo $CRUNTIME_HOME/start-vm -ip $ip/$smask -gw $gateway -tap $TAP -id $VMID -img docker.io/openwhisk/action-nodejs-v12:latest
 }
 
 function run_test_node {
-	curl --no-progress-meter -X POST $IP:8080/init -H 'Content-Type: application/json' -d @test-cruntime-nodejs/init.json
+	curl --no-progress-meter -X POST $ip:8080/init -H 'Content-Type: application/json' -d @test-cruntime-nodejs/init.json
 	for i in {1..3}
 	do
 		pretime
-		curl --no-progress-meter --max-time 5 -X POST $IP:8080/run -H 'Content-Type: application/json' -d @test-cruntime-nodejs/run.json
+		curl --no-progress-meter --max-time 5 -X POST $ip:8080/run -H 'Content-Type: application/json' -d @test-cruntime-nodejs/run.json
 		postime
 		done
 }
 
 function stop_node {
 	sudo $CRUNTIME_HOME/stop-vm -id $VMID
+	sudo bash $ARGO_HOME/lambda-manager/src/scripts/remove_taps.sh $TAP
 }
 
-# TODO - create tap and destroy tap
 cat hello-world.js | jq -sR  '{value: {main: "main", code: .}}' > test-cruntime-nodejs/init.json
 
-# Just to cache sudo permissions.
-sudo ls > /dev/null
-
-start_node #&> $tmpdir/nodejs.log
-
+start_node
 sleep 5
-
 run_test_node #> $tmpdir/nodejs.dat
 stop_node
