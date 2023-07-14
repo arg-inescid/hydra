e07ff5529c76e6751c5f9b03e3ee33ca17444f89
Rodrigo Bruno <rbruno@gsd.inesc-id.pt>
2023-07-14 19:07:14 +0100

    Updating prepare graalvm script.

--- graalvisor/docs/prepare-graalvm.sh
+++ graalvisor/docs/prepare-graalvm.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-function DIR {
-    echo "$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-}
+if [ -z "$JAVA_HOME" ]
+then
+    echo "Please set JAVA_HOME first. It you don't have GraalVM locally, download, extract, and set JAVA_HOME."
+    echo "> wget https://download.oracle.com/graalvm/17/latest/graalvm-jdk-17_linux-x64_bin.tar.gz"
+    echo "> tar -vzxf graalvm-jdk-17_linux-x64_bin.tar.gz"
+    echo "> export JAVA_HOME=$(pwd)/graalvm-jdk-17.0.7+8.1"
+    exit 1
+fi
 
-source $(DIR)/../lambda-manager/src/scripts/environment.sh
-
+# Install GraalVM components.
 $JAVA_HOME/bin/gu install native-image python nodejs
 
+# Install graalpy packages in virtual env.
 VENV=$JAVA_HOME/graalvisor-python-venv
-
-$JAVA_HOME/bin/graalpython -m venv $VENV
+$JAVA_HOME/bin/graalpy -m venv $VENV
 source $VENV/bin/activate
-
-graalpython -m ginstall list
-graalpython -m ginstall install numpy
-graalpython -m ginstall install Pillow
-graalpython -m ginstall install requests
-graalpython -m ginstall pypi texttable
-graalpython -m ginstall pypi igraph
+graalpy -m ginstall list
+graalpy -m ginstall install numpy
+graalpy -m ginstall install Pillow
+graalpy -m ginstall install requests
+graalpy -m ginstall install squiggle
+graalpy -m ginstall install torch
+graalpy -m ginstall install texttable
+graalpy -m ginstall install igraph
