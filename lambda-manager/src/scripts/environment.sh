1f559081d4cf2287475b24a915a99164878dc314
Rodrigo Bruno <rbruno@gsd.inesc-id.pt>
2023-07-25 01:14:50 +0100

    Minor updates to environment script.

--- lambda-manager/src/scripts/environment.sh
+++ lambda-manager/src/scripts/environment.sh
@@ -7,12 +7,10 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 export RES_HOME=$DIR/../../../resources
 export MANAGER_HOME=$DIR/../../../lambda-manager
 export NIUK_HOME=$DIR/../../../niuk
-export BENCHMARKS_HOME=$DIR/../../../../graalvm-argo-benchmarks
+export BENCHMARKS_HOME=$DIR/../../../benchmarks
 export PROXY_HOME=$DIR/../../../graalvisor
 export PROXY_JAR=$PROXY_HOME/build/libs/graalvisor-1.0-all.jar
 export CODEBASE_HOME=$DIR/../../codebase
 export GRAALVISOR_HOME=$PROXY_HOME/build/native-image
 export JAVA_HOME=$(realpath $RES_HOME/graalvm-ee-java11-22.1.0)
-# TODO - the KERNEL_PATH option is deprecated and will be removed.
-export KERNEL_PATH=$RES_HOME/vmlinux-4.14.35-1902.6.6.1.el7.container
 export CRUNTIME_HOME=$DIR/cruntime
