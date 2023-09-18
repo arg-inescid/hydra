8febc5926ed87187021c193a7c8eeb5890ae39a3
jgodinho <jorge.godinho@premium-minds.com>
2023-09-18 10:40:46 +0100

    fix error in native sandbox interface

--- graalvisor/src/main/c/NativeSandboxInterface.c
+++ graalvisor/src/main/c/NativeSandboxInterface.c
@@ -6,9 +6,6 @@
 #include "lazyisolation.h"
 #endif
 #include "org_graalvm_argo_graalvisor_sandboxing_NativeSandboxInterface.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 #include "network-isolation.h"
 
 #define PIPE_READ_END  0
@@ -86,6 +83,22 @@ JNIEXPORT int JNICALL Java_org_graalvm_argo_graalvisor_sandboxing_NativeSandboxI
     return pid;
 }
 
+JNIEXPORT void JNICALL Java_org_graalvm_argo_graalvisor_sandboxing_NativeSandboxInterface_createNativeIsolateSandbox(JNIEnv *env, jobject thisObj, jboolean lazyIsolation) {
+#ifdef LAZY_ISOLATION
+    if (lazyIsolation) {
+        install_thread_filter();
+    }
+#endif
+}
+
+JNIEXPORT void JNICALL Java_org_graalvm_argo_graalvisor_sandboxing_NativeSandboxInterface_createNativeRuntimeSandbox(JNIEnv *env, jobject thisObj, jboolean lazyIsolation) {
+#ifdef LAZY_ISOLATION
+    if (lazyIsolation) {
+        install_thread_filter();
+    }
+#endif
+}
+
 JNIEXPORT jint JNICALL Java_org_graalvm_argo_graalvisor_sandboxing_NativeSandboxInterface_switchToDefaultNetworkNamespace(JNIEnv *env, jobject thisObj) {
     return switchToDefaultNetworkNamespace();
 }
@@ -125,63 +138,4 @@ JNIEXPORT jint JNICALL Java_org_graalvm_argo_graalvisor_sandboxing_NativeSandbox
     int ret = disableVeths(ns_name);
     (*env)->ReleaseStringUTFChars(env, jName, ns_name);
     return ret;
-}
-
-JNIEXPORT int JNICALL Java_org_graalvm_argo_graalvisor_sandboxing_NativeSandboxInterface_createNativeProcessSandbox(JNIEnv *env, jobject thisObj, jintArray childPipeFD, jintArray parentPipeFD, jboolean lazyIsolation) {
-    int parentRead, parentWrite, childRead, childWrite;
-
-    // Preparing child pipe (where the child writes and the parent reads).
-    jint *childPipeFDptr = (*env)->GetIntArrayElements(env, childPipeFD, 0);
-    pipe(childPipeFDptr);
-    childRead = childPipeFDptr[PIPE_READ_END];
-    childWrite = childPipeFDptr[PIPE_WRITE_END];
-    (*env)->ReleaseIntArrayElements(env, childPipeFD, childPipeFDptr, 0);
-
-    // Preparing the parent pipe (where the parent writes and the child reads).
-    jint *parentPipeFDptr = (*env)->GetIntArrayElements(env, parentPipeFD, 0);
-    pipe(parentPipeFDptr);
-    parentRead = parentPipeFDptr[PIPE_READ_END];
-    parentWrite = parentPipeFDptr[PIPE_WRITE_END];
-    (*env)->ReleaseIntArrayElements(env, parentPipeFD, parentPipeFDptr, 0);
-
-    // Forking.
-    int pid = fork();
-    if (pid == 0) {
-        // Sanitizing the child process.
-        close_parent_fds(childWrite, parentRead);
-        reset_parent_signal_handlers();
-#ifdef LAZY_ISOLATION
-        if (lazyIsolation) {
-            install_proc_filter(childPipeFDptr);
-        }
-#endif
-    } else {
-        // Close the unnecessary pipe ends.
-        close(childPipeFDptr[PIPE_WRITE_END]);
-        if (!lazyIsolation) {
-            close(parentPipeFDptr[PIPE_READ_END]);
-        }
-#ifdef LAZY_ISOLATION
-        if (lazyIsolation) {
-            attach(pid, childPipeFDptr, parentPipeFDptr);
-        }
-#endif
-    }
-    return pid;
-}
-
-JNIEXPORT void JNICALL Java_org_graalvm_argo_graalvisor_sandboxing_NativeSandboxInterface_createNativeIsolateSandbox(JNIEnv *env, jobject thisObj, jboolean lazyIsolation) {
-#ifdef LAZY_ISOLATION
-    if (lazyIsolation) {
-        install_thread_filter();
-    }
-#endif
-}
-
-JNIEXPORT void JNICALL Java_org_graalvm_argo_graalvisor_sandboxing_NativeSandboxInterface_createNativeRuntimeSandbox(JNIEnv *env, jobject thisObj, jboolean lazyIsolation) {
-#ifdef LAZY_ISOLATION
-    if (lazyIsolation) {
-        install_thread_filter();
-    }
-#endif
 }
--- graalvisor/src/main/java/org/graalvm/argo/graalvisor/sandboxing/NativeSandboxInterface.java
+++ graalvisor/src/main/java/org/graalvm/argo/graalvisor/sandboxing/NativeSandboxInterface.java
@@ -6,6 +6,12 @@ public class NativeSandboxInterface {
 
     public static native void ginit();
 
+    public static native int createNativeProcessSandbox(int[] childPipe, int[] parentPipe, boolean lazyIsolation);
+
+    public static native void createNativeIsolateSandbox(boolean lazyIsolation);
+
+    public static native void createNativeRuntimeSandbox(boolean lazyIsolation);
+
     public static native int createNetworkNamespace(String jName, int jThirdByte, int jFourthByte);
 
     public static native int deleteNetworkNamespace(String jName);
@@ -17,10 +23,4 @@ public class NativeSandboxInterface {
     public static native int enableVeths(String jName);
 
     public static native int disableVeths(String jName);
-
-    public static native int createNativeProcessSandbox(int[] childPipe, int[] parentPipe, boolean lazyIsolation);
-
-    public static native void createNativeIsolateSandbox(boolean lazyIsolation);
-
-    public static native void createNativeRuntimeSandbox(boolean lazyIsolation);
 }
