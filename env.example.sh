37e9451a690cc278a57e5e17868a59d9a04d6414
RodrigoCidra <rodrasfc@gmail.com>
2024-01-09 15:36:42 +0000

    Add env sample.

---
+++ env.example.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export ARGO_HOME="path-to-home"
+export ERIM_HOME="path-to-home"
+export JAVA_HOME="path-to-home"
+export DEF_JAVA_HOME="path-to-home"
+export JAVASSIST_HOME="path-to-home"
--- env.sh
+++ env.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-export ARGO_HOME=/home/cidra/git/graalserverless
-export ERIM_HOME=/home/cidra/git/faastion/erim
-export JAVA_HOME=/home/cidra/software/graalvm-jdk-17.0.7+8.1
-export DEF_JAVA_HOME=/home/cidra/software/jdk-17.0.7
-export JAVASSIST_HOME=/home/cidra/git/faastion/native-execution/javassist
+export ARGO_HOME=$HOME/git/graalserverless
+export ERIM_HOME=$HOME/git/faastion/erim
+export JAVA_HOME=$HOME/software/graalvm-jdk-17.0.7+8.1
+export DEF_JAVA_HOME=$HOME/software/jdk-17.0.7
+export JAVASSIST_HOME=$HOME/git/faastion/native-execution/javassist
+export PATH="$HOME/software/musl-1.2.4/bin:$PATH"
