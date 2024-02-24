d5c346c4cbdec946689fa4ca28a384774a7c0598
Rodrigo Bruno <rbruno@dpss.inesc-id.pt>
2024-02-24 16:16:30 +0000

    Large code reorganization to better integrate w/ graalvisor (part 4).

--- graalvisor/src/main/c/svm-snapshot/main.c
+++ graalvisor/src/main/c/svm-snapshot/main.c
@@ -28,39 +28,42 @@
 #include <sys/time.h>
 #include <unistd.h>
 
-struct function_args {
+typedef struct {
     // ABI to create, invoke, destroy isolates.
     isolate_abi_t abi;
     // Pointer to the isolate.
     graal_isolate_t* isolate;
     // File descriptor used when installing seccomp.
     int seccomp_fd;
-    // List of memory mappings being tracked in the sandbox.
-    mapping_t mappings;
     // Path of the function library.
     char* function_path;
     // Function arguments.
-    void* args;
-    // Snapshot metadata file descriptor;
-    int meta_snap_fd;
-    // Snapshot memory file descriptor;
-    int mem_snap_fd;
+    void* function_args;
     // Integer used as a boolean to decide if the function has terminated.
     int finished;
-};
+} checkpoint_worker_args_t;
+
+typedef struct {
+     // ABI to create, invoke, destroy isolates.
+    isolate_abi_t* abi;
+    // Pointer to the isolate.
+    graal_isolate_t* isolate;
+} entrypoint_worker_args_t;
+
+
 
 enum EXECUTION_MODE { NORMAL, CHECKPOINT, RESTORE };
 
 // Wether we are checkpointing or restoreing.
 enum EXECUTION_MODE CURRENT_MODE = NORMAL;
 
-void run_serial_entrypoint(struct function_args* fargs, graal_isolatethread_t *isolatethread) {
+void run_serial_entrypoint(isolate_abi_t* abi, graal_isolatethread_t *isolatethread) {
      for (int i = 0; i < ENTRYPOINT_ITERS; i++) {
 #ifdef PERF
         struct timeval st, et;
         gettimeofday(&st, NULL);
 #endif
-        fargs->abi.entrypoint(isolatethread);
+        abi->entrypoint(isolatethread);
 #ifdef PERF
         gettimeofday(&et, NULL);
         log("entrypoint took %lu us\n", ((et.tv_sec - st.tv_sec) * 1000000) + (et.tv_usec - st.tv_usec));
@@ -68,64 +71,71 @@ void run_serial_entrypoint(struct function_args* fargs, graal_isolatethread_t *i
     }
 }
 
-void* run_parallel_entrypoint(void* args) {
-    struct function_args* fargs = (struct function_args*) args;
+void* entrypoint_worker(void* args) {
+    entrypoint_worker_args_t* wargs = (entrypoint_worker_args_t*) args;
     graal_isolatethread_t *isolatethread = NULL;
-    fargs->abi.graal_attach_thread(fargs->isolate, &isolatethread);
-    run_serial_entrypoint(fargs, isolatethread);
-    fargs->abi.graal_detach_thread(isolatethread);
+    wargs->abi->graal_attach_thread(wargs->isolate, &isolatethread);
+    run_serial_entrypoint(wargs->abi, isolatethread);
+    wargs->abi->graal_detach_thread(isolatethread);
     return NULL;
 }
 
-// TODO - we need to untangle this in two functions in cr.c.
-void* run_function(void* args) {
-    struct function_args* fargs = (struct function_args*) args;
-    graal_isolatethread_t *isolatethread = NULL;
-
-    if (CURRENT_MODE == CHECKPOINT) {
-        // Install seccomp filter.
-        fargs->seccomp_fd = install_seccomp_filter();
-        if (fargs->seccomp_fd < 0) {
-            err("error: failed install seccomp filter\n");
-            return NULL;
-        }
-    }
-
-    // If restoring, attach instead of creating a new isolate.
-    if (CURRENT_MODE == RESTORE) {
-        fargs->abi.graal_attach_thread(fargs->isolate, &isolatethread);
-    } else {
-        // Initialize abi.
-        if (load_function(fargs->function_path, &(fargs->abi))) {
-            err("error: failed to load isolate abi\n");
-            return NULL;
-        }
-
-        // Create isolate.
-        graal_create_isolate_params_t params;
-        memset(&params, 0, sizeof(graal_create_isolate_params_t));
-        if (fargs->abi.graal_create_isolate(&params, &(fargs->isolate), &isolatethread) != 0) {
-            err("error: failed to create isolate\n");
-            return NULL;
-        }
-    }
-
-    // Call function.
+void run_entrypoint(isolate_abi_t* abi, graal_isolate_t* isolate, graal_isolatethread_t* isolatethread) {
     if (ENTRYPOINT_CONC == 1) {
-        run_serial_entrypoint(fargs, isolatethread);
+        run_serial_entrypoint(abi, isolatethread);
     } else {
         pthread_t workers[ENTRYPOINT_CONC];
+        entrypoint_worker_args_t wargs = { .abi = abi, .isolate = isolate };
         for (int i = 0; i < ENTRYPOINT_CONC; i++) {
-            pthread_create(&(workers[i]), NULL, run_parallel_entrypoint, fargs);
+            pthread_create(&(workers[i]), NULL, entrypoint_worker, &wargs);
         }
         for (int i = 0; i < ENTRYPOINT_CONC; i++) {
             pthread_join(workers[i], NULL);
         }
     }
+}
+
+void run_function(char* function_path, isolate_abi_t* abi, graal_isolate_t** isolate) {
+    graal_isolatethread_t *isolatethread = NULL;
+
+    // Load function and initialize abi.
+    if (load_function(function_path, abi)) {
+        err("error: failed to load isolate abi\n");
+        return;
+    }
+
+    // Create isolate.
+    graal_create_isolate_params_t params;
+    memset(&params, 0, sizeof(graal_create_isolate_params_t));
+    if (abi->graal_create_isolate(&params, isolate, &isolatethread) != 0) {
+        err("error: failed to create isolate\n");
+        return;
+    }
+
+    // Run user code.
+    run_entrypoint(abi, *isolate, isolatethread);
 
     // Detach thread function isolate and quit.
-    fargs->abi.graal_detach_thread(isolatethread);
-    fargs->finished = 1;
+    abi->graal_detach_thread(isolatethread);
+}
+
+void* checkpoint_worker(void* args) {
+    checkpoint_worker_args_t* wargs = (checkpoint_worker_args_t*) args;
+
+    if (CURRENT_MODE == CHECKPOINT) {
+        // Install seccomp filter.
+        wargs->seccomp_fd = install_seccomp_filter();
+        if (wargs->seccomp_fd < 0) {
+            err("error: failed install seccomp filter\n");
+            return NULL;
+        }
+    }
+
+    // Prepare and run function.
+    run_function(wargs->function_path, &(wargs->abi), &(wargs->isolate));
+
+    // Mark execution as finished (will alert the seccomp monitor to quit).
+    wargs->finished = 1;
     return NULL;
 }
 
@@ -134,7 +144,7 @@ void usage_exit() {
     exit(1);
 }
 
-void init_args(struct function_args* fargs, int argc, char** argv) {
+char* init_args(int argc, char** argv) {
     if (argc != 3) {
         usage_exit();
     } else {
@@ -152,30 +162,73 @@ void init_args(struct function_args* fargs, int argc, char** argv) {
         default:
             usage_exit();
         }
-        fargs->function_path = argv[2];
     }
+    return argv[2];
 }
 
-int main(int argc, char** argv) {
-    struct function_args fargs;
-    pthread_t thread;
+void run_checkpoint(char* function_path, char* function_args, char* meta_snap_path, char* mem_snap_path) {
+    pthread_t worker;
 
-    // Disable buffering for stdout.
-    setvbuf(stdout, NULL, _IONBF, 0);
+    // Create and initialize the memory mappings list head;
+    mapping_t mappings;
+    memset(&mappings, 0, sizeof(mapping_t));
 
-    // Zero the entire argument data structure.
-    memset(&fargs, 0, sizeof(struct function_args));
+     // Create and initialize the checkpoint worker arguments struct.
+    checkpoint_worker_args_t wargs;
+    memset(&wargs, 0, sizeof(checkpoint_worker_args_t));
+    wargs.function_path = function_path;
+    wargs.function_args = function_args;
 
-    // Initialize based on arguments.
-    init_args(&fargs, argc, argv);
+    // If in checkpoint mode, open metadata file, wait for seccomp to be ready and handle notifications.
+    int meta_snap_fd = move_to_reserved_fd(open(meta_snap_path,  O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR));
+    int mem_snap_fd = move_to_reserved_fd(open(mem_snap_path,  O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR));
+    if (meta_snap_fd < 0 || mem_snap_fd < 0) {
+        err("error: failed to open meta (fd = %d) or memory (fd = %d) snapshot file\n", meta_snap_fd, mem_snap_fd);
+    } else {
+        meta_snap_fd = move_to_reserved_fd(meta_snap_fd);
+        mem_snap_fd = move_to_reserved_fd(mem_snap_fd);
+    }
 
-    // If in restore mode, start by restoring from the snapshot.
-    if (CURRENT_MODE == RESTORE) {
+    // Launch worker thread.
+    pthread_create(&worker, NULL, checkpoint_worker, &wargs); // TODO - move to clone3
+
+    // Wait while the thread initilizes and installs the seccomp filter.
+    while (!wargs.seccomp_fd) ; // TODO - avoid active waiting
+
+    // Move seccomp fd to a reserved fd.
+    wargs.seccomp_fd = move_to_reserved_fd(wargs.seccomp_fd);
+
+    // Keep handling syscall notifications.
+    handle_syscalls(wargs.seccomp_fd, &(wargs.finished), meta_snap_fd, &mappings);
+
+    // Join thread.
+    pthread_join(worker, NULL); // TODO - waitpid
+
+    // If in checkpoint mode, checkpoint memory.
+#ifdef DEBUG
+    print_proc_maps("before_checkpoint.log");
+    print_list(&mappings);
+#endif
+#ifdef PERF
+    struct timeval st, et;
+    gettimeofday(&st, NULL);
+#endif
+    checkpoint_memory(meta_snap_fd, mem_snap_fd, &mappings, &(wargs.abi), wargs.isolate);
+#ifdef PERF
+    gettimeofday(&et, NULL);
+    log("checkpoint took %lu us\n", ((et.tv_sec - st.tv_sec) * 1000000) + (et.tv_usec - st.tv_usec));
+#endif
+
+    close(meta_snap_fd);
+    close(mem_snap_fd);
+}
+
+void run_restore(char* meta_snap_path, char* mem_snap_path, isolate_abi_t* abi, graal_isolate_t** isolate) {
 #ifdef PERF
         struct timeval st, et;
         gettimeofday(&st, NULL);
 #endif
-        restore("metadata.snap", "memory.snap", &(fargs.abi), &(fargs.isolate));
+        restore("metadata.snap", "memory.snap", abi, isolate);
 #ifdef PERF
         gettimeofday(&et, NULL);
         log("restore took %lu us\n", ((et.tv_sec - st.tv_sec) * 1000000) + (et.tv_usec - st.tv_usec));
@@ -183,61 +236,35 @@ int main(int argc, char** argv) {
 #ifdef DEBUG
         print_proc_maps("after_restore.log");
 #endif
+}
+
+int main(int argc, char** argv) {
+    // Check args and get function path.
+    char* function_path = init_args(argc, argv);
+
+    // Disable buffering for stdout.
+    setvbuf(stdout, NULL, _IONBF, 0);
+
+    // If in restore mode, start by restoring from the snapshot.
+    if (CURRENT_MODE == RESTORE) {
+        graal_isolate_t* isolate;
+        graal_isolatethread_t *isolatethread = NULL;
+        isolate_abi_t abi;
+        run_restore("metadata.snap", "memory.snap", &abi, &isolate);
+        abi.graal_attach_thread(isolate, &isolatethread);
+        run_entrypoint(&abi, isolate, isolatethread);
+        abi.graal_detach_thread(isolatethread);
+    } else if (CURRENT_MODE == CHECKPOINT) {
+        run_checkpoint(function_path, NULL, "metadata.snap", "memory.snap");
+    } else {
+        graal_isolate_t* isolate;
+        isolate_abi_t abi;
+        run_function(function_path, &abi, &isolate);
     }
-
-    // Launch worker thread.
-    pthread_create(&thread, NULL, run_function, &fargs); // TODO - move to clone3
-
-    // If in checkpoint mode, open metadata file, wait for seccomp to be ready and handle notifications.
-    if (CURRENT_MODE == CHECKPOINT) {
-        int meta_snap_fd = move_to_reserved_fd(open("metadata.snap",  O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR));
-        int mem_snap_fd = move_to_reserved_fd(open("memory.snap",  O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR));
-        if (meta_snap_fd < 0 || mem_snap_fd < 0) {
-            err("error: failed to open meta (fd = %d) or memory (fd = %d) snapshot file\n", meta_snap_fd, mem_snap_fd);
-        } else {
-            fargs.meta_snap_fd = move_to_reserved_fd(meta_snap_fd);
-            fargs.mem_snap_fd = move_to_reserved_fd(mem_snap_fd);
-        }
-
-         // Wait while the thread initilizes and installs the seccomp filter.
-        while (!fargs.seccomp_fd) ; // TODO - avoid active waiting
-
-        fargs.seccomp_fd = move_to_reserved_fd(fargs.seccomp_fd);
-
-        // Keep handling syscall notifications.
-        handle_syscalls(fargs.seccomp_fd, &(fargs.finished), fargs.meta_snap_fd, &(fargs.mappings));
-    }
-
-    // Join thread.
-    pthread_join(thread, NULL); // TODO - waitpid
-
-    // If in checkpoint mode, checkpoint memory and isolate address.
-   if (CURRENT_MODE == CHECKPOINT) {
-#ifdef DEBUG
-        print_proc_maps("before_checkpoint.log");
-        print_list(&(fargs.mappings));
-#endif
-#ifdef PERF
-        struct timeval st, et;
-        gettimeofday(&st, NULL);
-#endif
-        checkpoint_memory(fargs.meta_snap_fd, fargs.mem_snap_fd, &(fargs.mappings), &(fargs.abi), fargs.isolate);
-#ifdef PERF
-        gettimeofday(&et, NULL);
-        log("checkpoint took %lu us\n", ((et.tv_sec - st.tv_sec) * 1000000) + (et.tv_usec - st.tv_usec));
-#endif
-        close(fargs.meta_snap_fd);
-        close(fargs.mem_snap_fd);
-   }
 
     // Flush any open streammed file.
     fflush(NULL);
 
     // Exit even if there are unfinished threads running in function code.
     exit(0);
-
-    // Tear down isolate after checkpointing.
-    graal_isolatethread_t *isolatethread = NULL;
-    fargs.abi.graal_attach_thread(fargs.isolate, &isolatethread);
-    fargs.abi.graal_tear_down_isolate(isolatethread);
 }
