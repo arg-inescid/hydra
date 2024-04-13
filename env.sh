e9d6da99a7938d20cb0e8b72402b022f64bf7d87
Cidra <Cidra@node.memisolation.serverless1-pg0.wisc.cloudlab.us>
2024-04-13 15:41:55 +0000

    Remove file.

--- env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export ARGO_HOME=$HOME/git/graalserverless
-export ERIM_HOME=$HOME/git/faastion/erim
-export JAVA_HOME=$HOME/software/graalvm-jdk-17.0.7+8.1
-export DEF_JAVA_HOME=$HOME/software/jdk-17.0.7
-export JAVASSIST_HOME=$HOME/git/faastion/native-execution/javassist
-export PATH="$HOME/software/musl-1.2.4/bin:$PATH"
