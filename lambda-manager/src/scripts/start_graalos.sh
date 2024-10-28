8083ab0c703fd61ed4cfd144ca13c5630ff65e38
Rodrigo Bruno <rodrigo.bruno@tecnico.ulisboa.pt>
2024-10-28 09:37:21 +0000

    Updating subproject paths.

--- lambda-manager/src/scripts/start_graalos.sh
+++ lambda-manager/src/scripts/start_graalos.sh
@@ -29,16 +29,12 @@ GRAALOS_PORT=$(($LAMBDA_PORT + $PORT_DISTANCE))
 LAMBDA_HOME="$CODEBASE_HOME"/"$LAMBDA_NAME"
 mkdir "$LAMBDA_HOME" &> /dev/null
 
-cd "$LAMBDA_HOME"
-
 docker run --rm --name $LAMBDA_NAME \
     -p $LAMBDA_PORT:9001 \
     -p $GRAALOS_PORT:$GRAALOS_PORT \
     -v "${GRAALOS_SDK_DIR}:/graalos" \
-    -v "/home/sergiyivan/work/graalos/graalos-benchmarks:/graalos-benchmarks" \
     -e GRAALOS_SDK_DIR="/graalos" \
-    -w "/graalos-benchmarks/graalos-client/" \
-    --user 1274:1145 \
+    -w "/graalos/benchmarks/graalos-client/" \
     $CONTAINER_IMAGE \
     ./run-graalhost-lm.sh $GRAALOS_PORT &
 
@@ -51,7 +47,7 @@ sleep 0.5
 
 res=""
 while [[ ! ${res} == *"AddEndpoint"* ]]; do
-    res=$(docker exec $LAMBDA_NAME curl --silent --show-error --write-out "%{http_code}" --data-binary '{ "act": "add_ep", "app": "/graalos-benchmarks/graalos-client/apps/simple-http/build/native/nativeCompile/simple-http", "ep": 2001, "default_socket": { "port": 9001 }, "listen_socket": { "port": 9001 }, "fsroot": "/", "fsmappings": [ { "concrete": "/", "virt": "/" } ], "env": { "myvar": "initial_value" }, "instances": 1 }' http://localhost:$GRAALOS_PORT/command)
+    res=$(docker exec $LAMBDA_NAME curl --silent --show-error --write-out "%{http_code}" --data-binary '{ "act": "add_ep", "app": "/graalos/benchmarks/graalos-client/apps/simple-http/build/native/nativeCompile/simple-http", "ep": 2001, "default_socket": { "port": 9001 }, "listen_socket": { "port": 9001 }, "fsroot": "/", "fsmappings": [ { "concrete": "/", "virt": "/" } ], "env": { "myvar": "initial_value" }, "instances": 1 }' http://localhost:$GRAALOS_PORT/command)
 done
 echo $res
 
