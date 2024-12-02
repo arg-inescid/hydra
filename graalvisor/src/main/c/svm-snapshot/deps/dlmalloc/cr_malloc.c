8ca3f1a4ecb08ebb412b075baca9e7e7e944e4f1
maksym <vasyl.lanko@tecnico.ulisboa.pt>
2024-12-02 13:32:06 +0000

    added calloc() and realloc(), and usage for mspace_inspect_all()

--- graalvisor/src/main/c/svm-snapshot/deps/dlmalloc/cr_malloc.c
+++ graalvisor/src/main/c/svm-snapshot/deps/dlmalloc/cr_malloc.c
@@ -5,7 +5,7 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-typedef void* mspace;
+//typedef void* mspace;
 
 typedef struct mspace_mapping {
     pid_t tid; // TODO - what to use here is not clear.
@@ -19,6 +19,8 @@ static mspace global = NULL;
 static __thread mspace local = NULL;
 // This global space
 static mspace_mapping_t mspace_table[1024];
+// test cr_malloc inspect_all example
+static int count = 0;
 
 void init_global_mspace() {
     mspace newmspace = create_mspace(0, 0);
@@ -51,14 +53,49 @@ void* malloc(size_t bytes) {
 }
 
 void free(void* mem) {
-    cr_printf(STDOUT_FILENO, "free %p\n", mem);
+    cr_printf(STDOUT_FILENO, "\t free %p\n", mem);
     return mspace_free(find_mspace(), mem);
+}
+
+void* calloc(size_t num, size_t size){
+    void* ret = mspace_calloc(find_mspace(), num, size);
+    cr_printf(STDOUT_FILENO, "calloc %d elements of %d size -> %p\n", num, size, ret);
+    return ret;
+}
+
+void* realloc(void* ptr, size_t size){
+    void* ret = mspace_realloc(find_mspace(), ptr, size);
+    cr_printf(STDOUT_FILENO, "realloc %p to have %d size -> %p\n", ptr, size, ret);
+    return ret;
 }
 
 void dumper(void* base, size_t size, unsigned int flags) {
     void* limit = ((char*)base) + size;
     cr_printf(STDOUT_FILENO, "segment %p - %p (len %lu) flags %d\n", base, limit, size, flags);
 
+}
+
+void count_chunks(void* start, void* end, size_t used, void* arg) {
+    if (used >= 100) ++count;
+}
+
+void inspect_chunks() {
+    // inspects all chunks, allocated and free)
+    mspace_inspect_all(global, count_chunks, NULL);
+    cr_printf(STDOUT_FILENO, "number of chunks: %d\n", count);
+    count = 0;
+}
+
+void print_mspace() {
+    struct malloc_state* mstate = (struct malloc_state *) global;
+    char* least_addr = mstate->least_addr;
+    size_t magic = mstate->magic;
+    cr_printf(STDOUT_FILENO, "Least addr from malloc_state: %p\n", least_addr);
+    cr_printf(STDOUT_FILENO, "Magic from malloc_state: %zu\n", magic);
+
+    size_t dvsize = mstate->dvsize;
+    mchunkptr dv = mstate->dv;
+    cr_printf(STDOUT_FILENO, "Next chunk will probably be on: %p , unless it's more than %zu bytes.\n", dv, dvsize);
 }
 
 void checkpoint_mspace() {
--- graalvisor/src/main/c/svm-snapshot/deps/dlmalloc/cr_malloc.h
+++ graalvisor/src/main/c/svm-snapshot/deps/dlmalloc/cr_malloc.h
@@ -2,15 +2,22 @@
 #define CR_MALLOC
 
 #include <stddef.h>   /* for size_t */
+#include "malloc_internal.h"
 
 // TODO - add the other memory allocated functions.
 void* malloc(size_t);
 void  free(void*);
+void* calloc(size_t num, size_t size);
+void* realloc(void* ptr, size_t size);
+
 
 // Checkpoints the mspace address. It is assumed that all memory was already included in the checkpoint.
 void checkpoint_mspace(); // TODO - receive fd, etc?
-
 void restore_mspace(); // TODO - add mspace to the list.
+
+// dev functions for testing
+void print_mspace();
+void inspect_chunks();
 
 // TODO - we need a function to add a particular thread to a new mspace
 // TODO - we also need a function to add a particular thread to an existing mspace
