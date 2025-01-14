f3cc3d33169211f5930eee96572fe72f45b00251
maksym <vasyl.lanko@tecnico.ulisboa.pt>
2025-01-14 12:47:33 +0000

    added functionality to cr_malloc library to use mspaces

--- graalvisor/src/main/c/svm-snapshot/deps/dlmalloc/cr_malloc.c
+++ graalvisor/src/main/c/svm-snapshot/deps/dlmalloc/cr_malloc.c
@@ -1,26 +1,57 @@
 #include "cr_malloc.h"
 #include "../../cr_logger.h"
-#include "malloc.h"
 #include <errno.h>
 #include <stdarg.h>
 #include <unistd.h>
-
-//typedef void* mspace;
-
-typedef struct mspace_mapping {
-    pid_t tid; // TODO - what to use here is not clear.
-    mspace mspace;
-
-} mspace_mapping_t;
+#include <sys/syscall.h> /* for syscall(__NR_gettid) */
 
 // Global memory pool.
 static mspace global = NULL;
 // Thread local reference to the memory pool that the thread should use (it can point to global).
 static __thread mspace local = NULL;
 // This global space
+
 static mspace_mapping_t mspace_table[1024];
-// test cr_malloc inspect_all example
+static family_t circular_notif_queue[MAX_NOTIFS];
+static int producer_counter = 0;
+static int consumer_counter = 0;
+static int mspace_counter = 5;
+static int first_entry = 1;
 static int count = 0;
+
+__thread int id = UNINITIALIZED;
+
+mspace_mapping_t* get_mspace_mapping() {
+    return mspace_table;
+}
+
+mspace get_mspace() {
+    // TODO: add seed as argument to return correct mspace
+    return mspace_table[5].mspace;
+}
+
+void join_mspace_when_inited(pid_t *future_child, pid_t parent) {
+    family_t family = {future_child, parent};
+    int position = producer_counter++ % MAX_NOTIFS;
+    circular_notif_queue[position] = family;
+}
+
+void inherit_mspace(pid_t child, pid_t parent) {
+    // TODO: get_id_from_tid(tid)
+    int found_id = 5;
+    mspace_mapping_t mapping = {child, mspace_table[found_id].mspace};
+    mspace_table[mspace_counter++] = mapping;
+    // mspace cur = mspace_table[found_id].mspace;
+    // cr_printf(STDOUT_FILENO, "share_mspace with mspace = %p found_id = %d\n", cur, found_id);
+}
+
+void consume_notif() {
+    int position = consumer_counter++ % MAX_NOTIFS;
+    family_t family = circular_notif_queue[position];
+    // int my_tid = syscall(__NR_gettid);
+    // cr_printf(STDOUT_FILENO, "consume_notif child tid = %d parent tid = %d @ MY_TID = %d\n", *family.child, family.parent, my_tid);
+    inherit_mspace(*family.child, family.parent);
+}
 
 void init_global_mspace() {
     mspace newmspace = create_mspace(0, 0);
@@ -33,17 +64,53 @@ void init_global_mspace() {
     }
 }
 
+void enter_mspace() {
+    id = mspace_counter++;
+    // if mspace for sandbox doesnt exist
+    mspace m = create_mspace(0, 0);
+    // TODO: use SEED to check if mspace for sandbox has already been created
+
+    int my_tid = syscall(__NR_gettid);
+    mspace_mapping_t mapping = {my_tid, m};
+    mspace_table[id] = mapping;
+    // cr_printf(STDOUT_FILENO, "enter_mspace id=%d has mspace=%p\n", id, mspace_table[id].mspace);
+}
+
 mspace find_mspace() {
-    if (local != NULL) {
-        return local;
-    } // TODO - else if check data structure
-    else {
-        if (global == NULL) {
-            init_global_mspace();
-        }
-        local = global;
+    // cr_printf(STDOUT_FILENO, "entered find_mspace() with id=%d global=%p\n", id, global);
+
+    // if there are notifs to handle
+    while (consumer_counter != producer_counter) {
+        consume_notif();
     }
-    return local;
+
+    if (first_entry == 1) {
+	    cr_printf(STDOUT_FILENO, "global == NULL !!!! \n");
+        init_global_mspace();
+        id = 0;
+        first_entry = 0;
+	    return global;
+    }
+
+    if (id == UNINITIALIZED) {
+        // TODO: get_id_from_tid(tid);
+        int result = 6;
+        id = result;
+        // mspace_mapping_t cur = mspace_table[id];
+        // cr_printf(STDOUT_FILENO, "id changed from -1 to %d\n", id);
+	    // cr_printf(STDOUT_FILENO, "cur.tid = %d cur.mspace = %p\n", cur.tid, cur.mspace);
+        return mspace_table[id].mspace;
+
+    } else if (id == 0 && global != NULL) {
+        // cr_printf(STDOUT_FILENO, "id=0 and global != NULL\n", id);
+	    return global;
+
+    } else if (id != UNINITIALIZED) {
+        // mstate cur = mspace_table[id].mspace;
+	    // cr_printf(STDOUT_FILENO, "mspace: %p\n", (mspace *) cur);
+        return mspace_table[id].mspace;
+    }
+    return global;
 }
 
 void* malloc(size_t bytes) {
@@ -84,18 +151,6 @@ void inspect_chunks() {
     mspace_inspect_all(global, count_chunks, NULL);
     cr_printf(STDOUT_FILENO, "number of chunks: %d\n", count);
     count = 0;
-}
-
-void print_mspace() {
-    struct malloc_state* mstate = (struct malloc_state *) global;
-    char* least_addr = mstate->least_addr;
-    size_t magic = mstate->magic;
-    cr_printf(STDOUT_FILENO, "Least addr from malloc_state: %p\n", least_addr);
-    cr_printf(STDOUT_FILENO, "Magic from malloc_state: %zu\n", magic);
-
-    size_t dvsize = mstate->dvsize;
-    mchunkptr dv = mstate->dv;
-    cr_printf(STDOUT_FILENO, "Next chunk will probably be on: %p , unless it's more than %zu bytes.\n", dv, dvsize);
 }
 
 void checkpoint_mspace() {
--- graalvisor/src/main/c/svm-snapshot/deps/dlmalloc/cr_malloc.h
+++ graalvisor/src/main/c/svm-snapshot/deps/dlmalloc/cr_malloc.h
@@ -2,21 +2,35 @@
 #define CR_MALLOC
 
 #include <stddef.h>   /* for size_t */
+#include "malloc.h"
 #include "malloc_internal.h"
+#include <sys/types.h>    //NOTE: Added
+
+#define MAX_NOTIFS 10
+#define UNINITIALIZED -1
+
+typedef struct mspace_mapping {
+    pid_t tid; // TODO - what to use here is not clear.
+    mspace mspace;
+} mspace_mapping_t;
+
+typedef struct family {
+    pid_t *child;
+    pid_t parent;
+} family_t;
 
 // TODO - add the other memory allocated functions.
-void* malloc(size_t);
-void  free(void*);
-void* calloc(size_t num, size_t size);
-void* realloc(void* ptr, size_t size);
 
-
-// Checkpoints the mspace address. It is assumed that all memory was already included in the checkpoint.
 void checkpoint_mspace(); // TODO - receive fd, etc?
 void restore_mspace(); // TODO - add mspace to the list.
 
+mspace_mapping_t* get_mspace_mapping();
+mspace get_mspace();
+void enter_mspace();
+void join_mspace_when_inited(pid_t*, pid_t);
+// void inherit_mspace(pid_t, pid_t);
+
 // dev functions for testing
-void print_mspace();
 void inspect_chunks();
 
 // TODO - we need a function to add a particular thread to a new mspace
