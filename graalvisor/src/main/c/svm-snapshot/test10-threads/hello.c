0af0df431c26f2f778d04d975bb568e0dfa9b209
maksym <vasyl.lanko@tecnico.ulisboa.pt>
2025-01-24 14:49:14 +0000

    explicitly create 2 threads for test10-threads

--- graalvisor/src/main/c/svm-snapshot/test10-threads/hello.c
+++ graalvisor/src/main/c/svm-snapshot/test10-threads/hello.c
@@ -6,8 +6,10 @@
 #include <stdlib.h>
 
 pthread_t worker = 0;
+pthread_t second = 0;
 
 void* run_function(void* args) {
+    printf("run_function\n");
     int myvar = 0;
     int tid = syscall(__NR_gettid);
     int pid = getpid();
@@ -37,9 +39,13 @@ void entrypoint(graal_isolatethread_t* thread, const char* fin, const char* fout
     fprintf(stderr, "[foreground thread] tid = %d pid = %d\n", tid, pid);
     if (worker == 0) {
         pthread_create(&worker, NULL, run_function, NULL);
-	worker = 0;
+	// worker = 0;
+    }
+    if (second == 0) {
+        pthread_create(&second, NULL, run_function, NULL);
     }
     sleep(1); // Give some time for the worker to do something.
+    return;
 }
 
 int graal_detach_thread(graal_isolatethread_t* thread) {
