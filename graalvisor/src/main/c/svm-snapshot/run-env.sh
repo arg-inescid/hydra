682a5ed16f17e5d1c6816caf419912e97f53c191
maksym <vasyl.lanko@tecnico.ulisboa.pt>
2025-02-21 16:18:25 +0000

    add test10 and remove unwanted double 'unshare'

--- graalvisor/src/main/c/svm-snapshot/run-env.sh
+++ graalvisor/src/main/c/svm-snapshot/run-env.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 # You can add any default options here
-UNSHARE_OPTS="unshare --map-root-user --keep-caps --mount-proc -f -p"
+UNSHARE_OPTS="--kill-child --map-root-user --keep-caps --mount-proc -f -p"
 ARCH_OPTS="-R"
 
 # Run the command with all arguments passed to the script
 unshare $UNSHARE_OPTS setarch $ARCH_OPTS "$@"
+#setarch $ARCH_OPTS unshare $UNSHARE_OPTS "$@"
