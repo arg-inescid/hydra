c38fe29f099bc49153a967188c12600113b5bf44
maksym <vasyl.lanko@tecnico.ulisboa.pt>
2025-05-29 11:47:43 +0100

    correct file to update

--- graalvisor/src/main/c/svm-snapshot/run-gdb-env.sh
+++ graalvisor/src/main/c/svm-snapshot/run-gdb-env.sh
@@ -10,4 +10,4 @@ ARCH_OPTS="-R"
 
 unshare $UNSHARE_OPTS \
 setarch $ARCH_OPTS \
-gdb -x gdb-commands.txt -ex "set environment LD_PRELOAD $(DIR)/deps/dlmalloc/hydralloc.so" --args $@
+gdb -x gdb-commands.txt -ex "set environment LD_PRELOAD $(DIR)/deps/dlmalloc/hydralloc.so" G_SLICE=malloc-always --args $@
