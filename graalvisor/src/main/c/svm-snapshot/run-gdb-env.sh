485c3fde9378726471f2b929700f9678ee81b9e8
maksym <vasyl.lanko@tecnico.ulisboa.pt>
2025-05-29 12:00:31 +0100

    place G_SLICE inside gdb env

--- graalvisor/src/main/c/svm-snapshot/run-gdb-env.sh
+++ graalvisor/src/main/c/svm-snapshot/run-gdb-env.sh
@@ -10,4 +10,4 @@ ARCH_OPTS="-R"
 
 unshare $UNSHARE_OPTS \
 setarch $ARCH_OPTS \
-gdb -x gdb-commands.txt -ex "set environment LD_PRELOAD $(DIR)/deps/dlmalloc/hydralloc.so" G_SLICE=always-malloc --args $@
+gdb -x gdb-commands.txt -ex "set environment LD_PRELOAD $(DIR)/deps/dlmalloc/hydralloc.so" -ex "set environment G_SLICE always-malloc" --args $@
