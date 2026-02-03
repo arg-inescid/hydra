d9dd34f04af38c2d3926932c4f0119a89cee15b8
Rodrigo Bruno <rodrigo.bruno@tecnico.ulisboa.pt>
2026-02-03 14:46:36 +0000

    Refactoring lambda connections and lambda pools. (#57)
    * Refactoring lambda connections and lambda pools.

--- lambda-manager/src/main/java/org/graalvm/argo/lambda_manager/pool/LambdaPool.java
+++ lambda-manager/src/main/java/org/graalvm/argo/lambda_manager/pool/LambdaPool.java
@@ -1,18 +1,53 @@
 package org.graalvm.argo.lambda_manager.pool;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 import org.graalvm.argo.lambda_manager.core.Function;
 import org.graalvm.argo.lambda_manager.core.Lambda;
+import org.graalvm.argo.lambda_manager.core.LambdaType;
 import org.graalvm.argo.lambda_manager.optimizers.LambdaExecutionMode;
 import org.graalvm.argo.lambda_manager.utils.LambdaConnection;
 
-public interface LambdaPool {
-    void setUp();
+public abstract class LambdaPool {
 
-    LambdaConnection nextLambdaConnection();
+    /**
+     * Type of lambdas that will be created.
+     */
+    protected final LambdaType lambdaType;
 
-    Lambda getLambda(LambdaExecutionMode mode, Function function);
+    /**
+     * Maximum number of network taps that will be setup by the Lambda Manager.
+     */
+    protected final int maxLambdas;
 
-    void disposeLambda(Lambda lambda);
+    /**
+     * Concurrent queue where the connections are pooled from.
+     */
+    protected final ConcurrentLinkedQueue<LambdaConnection> connectionPool;
 
-    void tearDown() throws InterruptedException;
+    /**
+     * Lambda pool used for amortization of lambda creation.
+     */
+    protected final Map<String, ConcurrentLinkedQueue<Lambda>> lambdaPool;
+
+    public LambdaPool(LambdaType lambdaType, int maxLambdas) {
+        this.lambdaType = lambdaType;
+        this.maxLambdas = maxLambdas;
+        this.connectionPool = new ConcurrentLinkedQueue<>();
+        this.lambdaPool = new ConcurrentHashMap<>();
+    }
+
+    public abstract void setUp();
+
+    public LambdaConnection nextLambdaConnection() {
+        return connectionPool.poll();
+    }
+
+    public abstract Lambda getLambda(LambdaExecutionMode mode, Function function);
+
+    public abstract void disposeLambda(Lambda lambda);
+
+    public abstract void tearDown() throws InterruptedException;
 }
